3dcb346429d96dbdf0a01b63d9c9ecb1a6f8384d
Daniel itallo <danielitallo@hotmail.com>
2020-05-03 20:36:10 -0300

    submit

--- instructors.js
+++ instructors.js
@@ -2,6 +2,12 @@ const fs = require("fs")
 const data = require("./data.json")
 const { age, date } = require("./utils")
 
+exports.index = function(req, res){
+
+
+    return res.render("instructors/index", { instructors: data.instructors })
+}
+
 //show
 exports.show = function(req,res){
     const { id } = req.params
@@ -39,7 +45,7 @@ exports.post = function(req, res){
     birth = Date.parse(birth)
     const created_at = Date.now()
     const id = Number(data.instructors.length + 1)
-
+   
     data.instructors.push({
         id,
         avatar_url,
@@ -54,6 +60,8 @@ exports.post = function(req, res){
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if(err) return res.send("Erro")
+
+        
 
         return res.redirect("/instructors")
     })
@@ -84,3 +92,56 @@ exports.edit = function(req, res){
     
     return res.render("instructors/edit",{ instructor  })
 }
+
+//put   
+
+exports.put = function(req, res){
+
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+       if( id == instructor.id){
+           index = foundIndex
+           return true
+       }
+    })
+
+    if (!foundInstructor) return res.send("Instrutor não encontrado")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2 ), function(err){
+        if(err) return res.send("Write error")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+    
+}
+
+//delete
+
+exports.delete = function(req, res){
+    const {id} = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Erro")
+
+        
+        return res.redirect("/instructors")
+    })
+}
+
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express") 
 const nunjucks = require("nunjucks") 
 const routes = require("./routes")
+const methodOverride = require("method-override")
 
 const server = express() //criando servidor ---- chamdno função express para dentro do servidor
 
 
 server.use(express.urlencoded({extended: true }))
 server.use(express.static("public"))
+server.use(methodOverride("_method"))
 server.use(routes)
+
 
 server.set("view engine", "njk") // configurando minha template engine
 
